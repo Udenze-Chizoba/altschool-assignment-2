3bafaa8556e68819a2b6f92f69c6a6ca6b18f5a4
Udenze Chizoba <109282369+Udenze-Chizoba@users.noreply.github.com>
2022-07-14 21:25:39 +0000

    . . .

--- src/assignment.js
+++ src/assignment.js
@@ -6,7 +6,7 @@ const assignment = {};
 
 function sumOfNumbers(arrayOfNumbers) {
   sum = 0;
-  for (let i = 0; i < arrayOfNumbers.length - 1; i++) {
+  for (let i = 0; i <= arrayOfNumbers.length - 1; i++) {
     sum+= arrayOfNumbers[i];
   }
   return sum;
@@ -15,20 +15,14 @@ function sumOfNumbers(arrayOfNumbers) {
 
 
 function countEvenNumbers(arrayOfNumbers) {
-  count = 0;
-  for (let j = 0; j < arrayOfNumbers.length - 1 ; j++) {
-    if (arrayOfNumbers[j] % 2 == 0) {
-      count+= 1 ;      
-    }
-  }
-    return count;    
+    return countevenNumbers;    
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
 
 function celsiusToFahrenheit(arrayOfNumbers) {
   const arrayOfNumbers2 = [];
-  for (let k = 0; k < arrayOfNumbers.length - 1; k++) {
+  for (let k = 0; k <= arrayOfNumbers.length - 1; k++) {
     arrayOfNumbers2[k] = Math.trunc(arrayOfNumbers[k] * 1.8 + 32)
     
   }
